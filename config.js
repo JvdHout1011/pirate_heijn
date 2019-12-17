69aa347ea89bb5f54fe80cf6384810ad03e76147
JvdHout1011 <jjdkhout@avans.nl>
2019-12-17 14:40:10 +0100

    full push

---
+++ config.js
@@ -0,0 +1,25 @@
+import * as firebase from 'firebase';
+import '@firebase/firestore';
+
+let config = {
+    apiKey: "AIzaSyBZklqnYeX6kTQSxclJIrnEFrXfz3nFvFQ",
+    authDomain: "supercoolgrootproject.firebaseapp.com",
+    databaseURL: "https://supercoolgrootproject.firebaseio.com",
+    projectId: "supercoolgrootproject",
+    storageBucket: "supercoolgrootproject.appspot.com",
+    messagingSenderId: "891217153878",
+    appId: "1:891217153878:web:d8d728cfd8b1e460f590f7",
+    measurementId: "G-Z91GJW39DN"
+
+};
+
+try{
+const firebaseApp = firebase.initializeApp(config);
+} catch(err) {
+    if( ! err.message.includes('already exists') ) {
+        throw err;
+    }
+}
+export const fb = firebase;
+console.log("FS:", fb.firestore);
+export const fs = firebase.firestore();
