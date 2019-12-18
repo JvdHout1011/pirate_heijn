2f18a45fbd1af65d962ae573078c6b444a4c3c87
Stijn de Vries <Stijn9@me.com>
2019-12-18 18:08:05 +0100

    Update name, add comments

--- cookieScanner.js
+++ cookieScanner.js
@@ -5,56 +5,56 @@ import {
 
 class LoginScreen extends Component {
   state = {
-    cookies    : {},
-    webViewUrl : ''
+    cookies: {},
+    webViewUrl: ''
   }
 
-  onNavigationStateChange = ( webViewState) => {
+  onNavigationStateChange = (webViewState) => {
     const { url } = webViewState;
 
     // when WebView.onMessage called, there is not-http(s) url
-    if(url.includes('http')) {
+    if (url.includes('http')) {
       this.setState({ webViewUrl: url })
     }
   }
 
+  // Checks for specified cookies from all saved cookies
   _checkNeededCookies = () => {
-    const { cookies, webViewUrl} = this.state;
+    const { cookies, webViewUrl } = this.state;
     let ahTokenValue = ''
     let cookieName = 'ah_token'
 
-    console.log(ahTokenValue) 
-      if (cookies[cookieName]) {
-        alert(cookieName+"'s value = " + cookies[cookieName]);
-        ahTokenValue = cookies[cookieName]
-        console.log(ahTokenValue)
-      }else{
-        console.log("token not found")
-      }
+    console.log(ahTokenValue)
+
+    if (cookies[cookieName]) {
+      alert(cookieName + "'s value = " + cookies[cookieName]);
+      ahTokenValue = cookies[cookieName]
+      console.log(ahTokenValue)
+    } else {
+      console.log("token not found")
+    }
   }
 
+  // Splits, orders and saves all cookies
   _onMessage = (event) => {
-    
     const { data } = event.nativeEvent;
-    const cookies  = data.split(';'); // `csrftoken=...; rur=...; mid=...; somethingelse=...`
-    
-    cookies.forEach((cookie) => {
-      
-      const c = cookie.trim().split('=');
+    const cookies = data.split(';');
 
+    cookies.forEach((cookie) => {
+      const c = cookie.trim().split('=');
       const new_cookies = this.state.cookies;
       new_cookies[c[0]] = c[1];
-      
+
       this.setState({ cookies: new_cookies });
-      console.log( ">>>>", new_cookies )
+      console.log(">>>>", new_cookies)
     });
-    
+
     this._checkNeededCookies();
   }
 
   render() {
     const jsCode = "window.postMessage(document.cookie)"
-    // let jsCode = "window.postMessage(document.cookie= 'login=; expires=Bla bla bla')"; // if you need to write some cookies, not sure if it goes to shared cookies, most probably no :)
+    // This will send the cookies to the onMessage in the WebView.
 
     return (
       <WebView
@@ -63,7 +63,7 @@ class LoginScreen extends Component {
         onMessage={this._onMessage}
         injectedJavaScript={jsCode}
         style={{ flex: 1 }}
-        />
+      />
     );
   }
 }
