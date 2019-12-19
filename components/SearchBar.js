60e2474d840ab59589b17851600e6c51b9117b7a
Loïs Berndsen <me@loisberndsen.nl>
2019-12-19 13:31:19 +0100

    visualised search bar, can get data but not yet in array

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -10,12 +10,16 @@ export default class SearchBar extends React.Component {
         const getSearchList = fs.collection("products").where("productName", "==", searchTerm);
 
         const querySnapshot = await getSearchList.get();
-        console.log(querySnapshot);
+        // console.log(querySnapshot);
 
         // querySnapshot is an array with documents which it got from the search query.
         // To see the data from this array you have to map the data using doc.data().
         // If you don't map you get documents, if you do map you get arrays with data.
-        const products = querySnapshot.map(doc => doc.data());
+        // querySnapshot.map(doc => doc.data());
+        const products = querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+        });
+
         console.log("Your search found: " + products);
     };
 
@@ -46,7 +50,7 @@ export default class SearchBar extends React.Component {
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity style={styles.button} color="#00ade6" onPress={this.buttonPressHandler}>
                         {/*<Text style={{color: '#FFFFFF', fontSize: 20, letterSpacing: 3}}>ZOEK</Text>*/}
-                        <Image style={styles.searchIcon} source={require('assets/searchIcon.png')}/>
+                        <Image style={styles.searchIcon} source={require('../assets/searchIcon.png')}/>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -56,18 +60,36 @@ export default class SearchBar extends React.Component {
 
 const styles = StyleSheet.create({
     inputContainer: {
-
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#eeeeee',
+        flexDirection: 'row'
     },
     input: {
-
+        backgroundColor: '#333333',
+        color: '#eeeeee',
+        padding: 5,
+        paddingLeft: 10,
+        borderBottomLeftRadius: 10,
+        borderTopLeftRadius: 10,
+        width: '85%',
+        height: 40
     },
     buttonContainer: {
-
+        width: '15%',
+        height: 40,
+        backgroundColor: 'pink',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        borderBottomRightRadius: 10,
+        borderTopRightRadius: 10,
     },
     button: {
-
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     searchIcon: {
-
+        width: 15,
+        height: 15
     }
 });
