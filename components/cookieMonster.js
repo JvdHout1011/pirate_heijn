3177f25c2b151a1503beff16a01ee103a766afeb
JvdHout1011 <jjdkhout@avans.nl>
2020-01-09 12:41:07 +0100

    next try

--- components/cookieMonster.js
+++ components/cookieMonster.js
@@ -1,34 +1,40 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
+import { fb, fs } from '../config.js';
 
-export default class CookieReceiver extends Component {
+export default class LoginScreen extends Component {
   state = {
     cookies: {},
     webViewUrl: ''
   }
 
-  sendData = (cookies) => {
-    this.props.parentCallback(cookies);
+  sendData = (koekjes) => {
+    if(this.state.webViewUrl === "https://www.ah.nl/mijn/dashboard/"){
+    this.props.parentCallback(koekjes);
+  } else {
+    return
+  }
   }
 
   onNavigationStateChange = (webViewState) => {
     const { url } = webViewState;
     if (url.includes('http')) {
       this.setState({ webViewUrl: url })
+      
     }
   }
 
   // Splits, orders and saves all cookies to the state
   onMessage = (event) => {
-    if(this.state.webViewUrl == "https://www.ah.nl/mijn/dashboard/"){
+    if(this.state.webViewUrl === "https://www.ah.nl/mijn/dashboard/"){
     const { data } = event.nativeEvent;
     this.sendData("'" + data + "'")
     console.log(data)
   } else {
-    return;
+    return
   }
   }
-
+  
   // The Navigation bar
   static navigationOptions = ({ navigation }) => {
     const { params = {} } = navigation.state;
