91ce692d6a26a185628246988b43f0ad2f99d986
Stijn de Vries <Stijn9@me.com>
2020-01-09 09:39:26 +0100

    Revert "Revert "experimented with views not yet complete""
    This reverts commit 2933a827f99f238a52f55fbca6c44b6808770fdb.

--- components/ScreenPages/Element/productViewComponent.js
+++ components/ScreenPages/Element/productViewComponent.js
@@ -16,72 +16,63 @@ import { Card,  } from 'react-native-paper';
 
 
 // Sreen page layout with logic
-class ProductScreen extends React.Component {
+class Product extends React.Component {
   
-    static navigationOptions = {
-      title: 'Products',   
-  };
-
- }
+    
+ 
     render() {
-      return (
-        <ScrollView>
-        <View style={{ flex: 1, alignItems: 'center',  }}>
-          <Text style={styles.text}>
-            Search
-            </Text>
+        return (
             <TouchableOpacity
-         style={buttons.button}
-         onPress={() => this.props.navigation.navigate('Search')}
-       >
-         <Text style={buttons.buttonText}> Go to Search </Text>
-          </TouchableOpacity>
-
-          <View style={productView.boxSize}>
-            <View style={{
-        flex: 1,
-                flexDirection: 'row',
-        flexWrap:'wrap',
-                justifyContent: 'space-between',
-                alignItems: "flex-start"
+					onPress={() => this.props.navigation.navigate("Product")}
+				> 
+            <View style={pageSetup.Plasing}>
+             
+                    <View style={productView.boxSize}>
+                        <View style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                            justifyContent: 'space-between',
+                            alignItems: "flex-start"
         
-      }}>
-          <Image
-           style={image.size}
-           source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
-              />
-              </View>
+                        }}>
+                            <Image
+                                style={image.size}
+                                source={{ uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' }}
+                            />
+                        </View>
               
-              <View style={{
-                    flex: 2,
-                    flexDirection: 'column',
-                    padding: 10,
-                    alignContent: 'flex-end'
+                        <View style={{
+                            flex: 2,
+                            flexDirection: 'column',
+                            padding: 10,
+                            alignContent: 'flex-end'
         
-      }}>
-          <Text>
-                  Hier in komt alle infotmatie van het product!{'\n'}{'\n'} hee
-                        
+                        }}>
+                            <Text>
+                                Hier in komt alle infotmatie van het product!{'\n'}{'\n'} hee
+                                      
           </Text>
-          <View style={{
+                            <View style={{
         
-                 alignItems: "flex-end",
-        flexDirection:"column-reverse",
-      }}>
-          <Text style={productView.productPrice}>
-            €5,-
+                                alignItems: "flex-end",
+                                flexDirection: "column-reverse",
+                            }}>
+                                <Text style={productView.productPrice}>
+                                    €5,-
           </Text>
-          </View>
-              </View>
-            </View>
-        </View>
-        </ScrollView>
+                            </View>
+                        </View>
+                    </View>
+                 
+                </View>
+           </TouchableOpacity>
        
-      );
+        );
     }
-
+}
   
  
 
 
-export default ProductScreen
+export default Product
