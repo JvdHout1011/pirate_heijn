dead271473ad1e90cbb0429ce5ac54710cb7fd67
JvdHout1011 <jjdkhout@avans.nl>
2020-01-13 13:55:26 +0100

    stopped account flooding

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,49 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+// const firestore = require("../node_modules/@firebase/firestore");
 
+let config = {
+	apiKey: "AIzaSyBZklqnYeX6kTQSxclJIrnEFrXfz3nFvFQ",
+	authDomain: "supercoolgrootproject.firebaseapp.com",
+	databaseURL: "https://supercoolgrootproject.firebaseio.com",
+	projectId: "supercoolgrootproject",
+	storageBucket: "supercoolgrootproject.appspot.com",
+	messagingSenderId: "891217153878",
+	appId: "1:891217153878:web:d8d728cfd8b1e460f590f7",
+	measurementId: "G-Z91GJW39DN",
+};
+admin.initializeApp(functions.config().firebase);
+
+let fs = admin.firestore();
+
+try {
+	const firebaseApp = admin.initializeApp(config);
+} catch (err) {
+	if (!err.message.includes("already exists")) {
+		throw err;
+	}
+}
+
+async function jada() {
+    console.log("start van functie");
+    const array = [];
+    const test = fs.collection("products");
+    const result = await test.get();
+    console.log("data ophalen");
+    result.then(result => {
+        const documents = result.docs;
+        documents.forEach(doc => {
+            array.push(doc.data());
+           console.log(doc.data());
+         })   
+        return array;
+})
+}
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.productsFunction = functions.https.onRequest((request, response) => {
+ response.send("I told you.");
+ jada();
+ console.log("query succesfull")
+});
