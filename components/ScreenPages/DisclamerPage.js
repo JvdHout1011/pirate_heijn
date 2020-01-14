307619e42efb44d4d2ad593e0a8073bc676bf07b
Stijn de Vries <Stijn9@me.com>
2020-01-14 12:31:53 +0100

    Restore bypass

--- components/ScreenPages/DisclamerPage.js
+++ components/ScreenPages/DisclamerPage.js
@@ -49,62 +49,70 @@ export default class DisclamerScreen extends React.Component {
 	render() {
 		// startGetSessionCookie();
 		return (
-			<React.Fragment>
-				<View style={pageSetup.Plasing}>
-						<Text style={text.h1}>Welkom bij Pirate Heijn {"\n"}</Text>
-						<Text style={{ textAlign: "center" }}>
-							Met Pirate Heijn kan je nog meer Bonus voordeel krijgen door jouw
-							aanbiedigen te delen: een voor allen, allen voor een.
-							{"\n"}
-							{"\n"}
-							Zo werkt het:
-							{"\n"}
-							{"\n"}
-							1. Log in met jouw AH account
-							{"\n"}
-							{"\n"}
-							2. Kies welke bonus aanbiedingen je wilt gebruiken
-							{"\n"}
-							{"\n"}
-							3. Scan de Bonus Kaarten bij de kassa
-							{"\n"}
-							{"\n"}
-						</Text>
+      <React.Fragment>
+        <View style={pageSetup.Plasing}>
+          <Text style={text.h1}>Welkom bij Pirate Heijn {'\n'}</Text>
+          <Text style={{ textAlign: 'center' }}>
+            Met Pirate Heijn kan je nog meer Bonus voordeel krijgen door jouw
+            aanbiedigen te delen: een voor allen, allen voor een.
+            {'\n'}
+            {'\n'}
+            Zo werkt het:
+            {'\n'}
+            {'\n'}
+            1. Log in met jouw AH account
+            {'\n'}
+            {'\n'}
+            2. Kies welke bonus aanbiedingen je wilt gebruiken
+            {'\n'}
+            {'\n'}
+            3. Scan de Bonus Kaarten bij de kassa
+            {'\n'}
+            {'\n'}
+          </Text>
 
-						<Text style={text.h2}>Privacy</Text>
-						<Text style={{ textAlign: "center" }}>
-							Om jouw aanbiedingen op te halen moet je inloggen bij Albert
-							Heijn, wij slaan jouw wachtwoord niet op. Wij slaan alleen jouw
-							aanbiedigen op, deze zijn gelinkt aan jouw bonuskaartnummer.
-							{"\n"}
-							{"\n"}
-							Pirate Heijn is geen onderdeel van Albert Heijn
-						</Text>
+          <Text style={text.h2}>Privacy</Text>
+          <Text style={{ textAlign: 'center' }}>
+            Om jouw aanbiedingen op te halen moet je inloggen bij Albert Heijn,
+            wij slaan jouw wachtwoord niet op. Wij slaan alleen jouw aanbiedigen
+            op, deze zijn gelinkt aan jouw bonuskaartnummer.
+            {'\n'}
+            {'\n'}
+            Pirate Heijn is geen onderdeel van Albert Heijn
+          </Text>
 
-						<TouchableOpacity
-							style={buttons.button}
-							onPress={() => this.props.navigation.navigate("LogIn")}
-						>
-							<Image
-								source={require("../../assets/icons/ShieldLock.png")}
-								fadeDuration={0}
-								style={buttons.buttonImage}
-							/>
-							<Text style={buttons.buttonText}> Inloggen bij AH → </Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-							style={buttons.button}
-							onPress={() => this.checkForAuthenticated}
-						>
-							<Image
-								source={require("../../assets/icons/ShieldLock.png")}
-								fadeDuration={0}
-								style={buttons.buttonImage}
-							/>
-							<Text style={buttons.buttonText}> Bypass login → </Text>
-						</TouchableOpacity>
-					</View>
-			</React.Fragment>
-		);
+          <TouchableOpacity
+            style={buttons.button}
+            onPress={() => this.props.navigation.navigate('LogIn')}>
+            <Image
+              source={require('../../assets/icons/ShieldLock.png')}
+              fadeDuration={0}
+              style={buttons.buttonImage}
+            />
+            <Text style={buttons.buttonText}> Inloggen bij AH → </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={buttons.button}
+            onPress={() => this.checkForAuthenticated}>
+            <Image
+              source={require('../../assets/icons/ShieldLock.png')}
+              fadeDuration={0}
+              style={buttons.buttonImage}
+            />
+            <Text style={buttons.buttonText}> Check login → </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={buttons.button}
+            onPress={() => this.props.navigation.navigate('Home')}>
+            <Image
+              source={require('../../assets/icons/ShieldLock.png')}
+              fadeDuration={0}
+              style={buttons.buttonImage}
+            />
+            <Text style={buttons.buttonText}> Bypass login → </Text>
+          </TouchableOpacity>
+        </View>
+      </React.Fragment>
+    );
 	}
 }
