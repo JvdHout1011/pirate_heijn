c6d9e212c2248fb86dd972b6908c70f7fb1503af
Loïs Berndsen <me@loisberndsen.nl>
2020-01-16 11:10:57 +0100

    refactored and styled settingspage

--- components/ScreenPages/SettingsPage.js
+++ components/ScreenPages/SettingsPage.js
@@ -1,128 +1,31 @@
 import * as React from "react";
-import {
-	Text,
-	View,
-	StyleSheet,
-	Button,
-	TextInput,
-	TouchableOpacity,
-	Image,
-} from "react-native";
-import {
-	styles,
-	buttons,
-	textInput,
-	pageSetup,
-	productView,
-	image,
-	text,
-} from "./StylesPage";
-import { fb, fs } from "../../config.js";
+import {Text, View, TouchableOpacity} from "react-native";
+import {buttons, pageSetup, text} from "./StylesPage";
 
-// Screen page layout with logic
 class SettingsScreen extends React.Component {
 	static navigationOptions = {
 		title: "Instellingen",
 	};
+
 	render() {
 		return (
-			// 	<View style={pageSetup.Plasing}>
-
-			// 		<View style={productView.boxSize}>
-
-			//                 <View style={{
-			//                     flex: 1,
-			//                     flexDirection: 'row',
-			//                     flexWrap: 'wrap',
-			//                     justifyContent: 'space-between',
-			//                     alignItems: "flex-start"
-
-			//                 }}>
-			//                     <Image
-			//                         style={image.size}
-			//                         source={{ uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' }}
-			//                     />
-			//                 </View>
-
-			//                 <View style={{
-			//                     flex: 2,
-			//                     flexDirection: 'column',
-			//                     padding: 10,
-			//                     alignContent: 'flex-end'
-
-			//                 }}>
-			//                     <Text>
-			//                         Hier in komt alle infotmatie van het product!{'\n'}{'\n'} hee
-
-			//   </Text>
-			//                     <View style={{
-
-			//                         alignItems: "flex-end",
-			//                         flexDirection: "column-reverse",
-			//                     }}>
-			//                         <Text style={productView.productPrice}>
-			//                             €5,-
-			//   </Text>
-			//                     </View>
-			//                 </View>
-			//             </View>
-			//         </View>
 			<View style={pageSetup.Placing}>
-				<Text style={text.alertText}>Deze pagina wordt niet gebruikt!</Text>
-				<Text style={{ textAlign: 'center' }}>
-					Hier valt niks te zoeken dit is namelijkeen dummie pagina.
-					{'\n'}
-					{'\n'}
-					Deze app is bedoelt als prototype.
+				<Text style={text.h1}>Deze pagina wordt niet gebruikt!</Text>
+				<Text style={
+					{
+						textAlign: 'left',
+						padding: 25
+					}}>
+					Hier valt niks te zoeken op het moment, dit is een dummie pagina!
+					Deze app is bedoeld als prototype.
 					</Text>
 				
 				<TouchableOpacity
 					style={buttons.button}
 					onPress={() => this.props.navigation.navigate("Home")}
 				>
-					<Text style={buttons.buttonText}> Terug naar Pirate Hein </Text>
+					<Text style={buttons.buttonText}>Terug naar Pirate Heijn</Text>
 				</TouchableOpacity>
-
-				{/* <TouchableOpacity
-					onPress={() => this.props.navigation.navigate("Product")}
-				>
-					<View style={productView.boxSize}>
-						<View
-							style={{
-								flex: 1,
-								flexDirection: "row",
-								flexWrap: "wrap",
-								justifyContent: "space-between",
-								alignItems: "flex-start",
-							}}
-						>
-							<Image
-								style={image.productSize}
-								source={{
-									uri: "https://stijndv.com/images/PirateHeinWhite.png",
-								}}
-							/>
-						</View>
-						<View
-							style={{
-								flex: 2,
-								flexDirection: "column",
-								alignContent: "flex-end",
-							}}
-						>
-							<Text style={text.h3}>Geen product beschikbaar</Text>
-							<Text>Probeer iets anders te zoeken</Text>
-							<View
-								style={{
-									alignItems: "flex-end",
-									flexDirection: "column-reverse",
-								}}
-							>
-								<Text style={productView.productPrice}>€ --</Text>
-							</View>
-						</View>
-					</View>
-				</TouchableOpacity> */}
 			</View>
 		);
 	}
