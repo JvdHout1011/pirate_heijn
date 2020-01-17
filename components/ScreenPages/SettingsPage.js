5538820769b4bf0c966a56b03f3e38b93f3480b8
Loïs Berndsen <me@loisberndsen.nl>
2020-01-17 11:57:00 +0100

    refactor settings page

--- components/ScreenPages/SettingsPage.js
+++ components/ScreenPages/SettingsPage.js
@@ -1,123 +1,117 @@
 import * as React from 'react';
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  YellowBox,
-  TextInput,
-  ScrollView
-} from 'react-native';
-import { account, buttons, text, styles } from './StylesPage';
-import { fs } from '../../config';
+import {Text, View, TouchableOpacity, YellowBox, TextInput, ScrollView} from 'react-native';
+import {account, buttons, text, styles} from './StylesPage';
+import {fs} from '../../config';
 import Barcode from './packages/react-native-barcode-builder';
 
 class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Account'
-  };
-
-  constructor(props) {
-    super(props);
-    console.ignoredYellowBox = ['Setting a timer'];
-    YellowBox.ignoreWarnings(['Setting a timer']);
-    this.state = {
-      cardNr: '',
-      airMilesNr: '',
-      docNr: '',
-      cookie: ''
+    static navigationOptions = {
+        title: 'Account'
     };
 
-    startGetSessionCookie = async () => {
-      AsyncStorage.getItem('auth_cookie').then(value => {
-        //AsyncStorage returns a promise so adding a callback to get the value
-        this.setState({ cookie: value });
-      });
+    constructor(props) {
+        super(props);
+        console.ignoredYellowBox = ['Setting a timer'];
+        YellowBox.ignoreWarnings(['Setting a timer']);
+        this.state = {
+            cardNr: '',
+            airMilesNr: '',
+            docNr: '',
+            cookie: ''
+        };
+
+        startGetSessionCookie = async () => {
+            AsyncStorage.getItem('auth_cookie').then(value => {
+                // AsyncStorage returns a promise so adding a callback to get the value
+                this.setState({ cookie: value });
+            });
+        };
+
+        getDiscountCardnumber = async () => {
+            const getBonuskaartNumber = fs.collection('users').where('id', '==', 1);
+            const firstResult = await getBonuskaartNumber.get();
+            const docResult = firstResult.docs;
+
+            if (firstResult === '' || firstResult === null || firstResult.empty) {
+                console.log('empty result');
+            }
+
+            docResult.forEach(doc => {
+                let actualCardNr = doc.data().bonuskaart_number;
+                this.setState({ cardNr: actualCardNr });
+            });
+
+            return firstResult;
+        };
+
+        getDiscountCardnumber().then(actualCardNr => {});
+    }
+
+    retrieveDoc = async () => {
+        const query = fs.collection('users').where('id', '==', userID);
+        const QuerySnapshot = await query.get();
+        QuerySnapshot.forEach(doc => {
+            console.log(doc.id);
+            this.setState({ docNr: doc.id });
+        });
     };
 
-    getDiscountCardnumber = async () => {
-      const getBonuskaartNumber = fs.collection('users').where('id', '==', 1);
-      const firstResult = await getBonuskaartNumber.get();
-      const docResult = firstResult.docs;
-      if (firstResult === '' || firstResult === null || firstResult.empty) {
-        console.log('empty result');
-      }
+    startSetAirMilesCardNumber = async () => {
+        this.retrieveDoc();
+        let getDiscountCardNumberDoc = fs.collection('users').doc(this.state.docNr);
+        let newAirMilesNumber = this.state.airMilesNr;
 
-      docResult.forEach(doc => {
-        let actualCardNr = doc.data().bonuskaart_number;
-        this.setState({ cardNr: actualCardNr });
-      });
-
-      return firstResult;
+        const query = await getDiscountCardNumberDoc.update({
+            airmileskaart_number: newAirMilesNumber
+        });
     };
 
-    getDiscountCardnumber().then(actualCardNr => {});
-  }
+    UNSAFE_componentWillMount() {
+        // dD this instead
+        startGetSessionCookie();
+    }
 
-  retrieveDoc = async () => {
-    const query = fs.collection('users').where('id', '==', userID);
-    const QuerySnapshot = await query.get();
-    QuerySnapshot.forEach(doc => {
-      console.log(doc.id);
-      this.setState({ docNr: doc.id });
-    });
-  };
-
-  startSetAirMilesCardNumber = async () => {
-    this.retrieveDoc();
-    let getDiscountCardNumberDoc = fs.collection('users').doc(this.state.docNr);
-    let newAirMilesNumber = this.state.airMilesNr;
-
-    const query = await getDiscountCardNumberDoc.update({
-      airmileskaart_number: newAirMilesNumber
-    });
-  };
-
-  UNSAFE_componentWillMount() {
-    // do this instead
-    startGetSessionCookie();
-  }
-
-  render() {
-    return (
-      <ScrollView style={account.container}>
-        <Text style={[text.h1, text.textAlignCenter]}>Jouw bonuskaart</Text>
-        <View style={account.bonuskaartStyling}>
-          <View
-            style={{
-              maxWidth: '100%',
-              flex: 1,
-              backgroundColor: '#FFF',
-              padding: 10,
-              borderRadius: 10
-            }}>
-            <Barcode value="2620682025269" format="EAN13" flat />
-            <Text style={text.monospace}>{this.state.cardNr}</Text>
-          </View>
-        </View>
-        <Text style={[text.h1, text.textAlignCenter, account.extraMargin]}>
-          Jouw Air Miles kaart
-        </Text>
-        <View style={account.inputPlacement}>
-          <TextInput
-            placeholder={'Vul Air Miles kaartnummer in...'}
-            style={account.input}
-            onChangeText={airMilesNr => this.setState({ airMilesNr })}
-            onSubmitEditing={this.startSetAirMilesCardNumber()}
-            placeholderTextColor="#838383"
-            selectionColor="#ff7900"
-            clearButtonMode="always"
-            returnKeyType="go"
-            keyboardType="decimal-pad"
-          />
-        </View>
-        <TouchableOpacity
-          style={buttons.button}
-          onPress={this.startSetAirMilesCardNumber()}>
-          <Text style={buttons.buttonText}>Aanpassen</Text>
-        </TouchableOpacity>
-      </ScrollView>
-    );
-  }
+    render() {
+        return (
+            <ScrollView style={account.container}>
+                <Text style={[text.h1, text.textAlignCenter]}>Jouw bonuskaart</Text>
+                <View style={account.bonuskaartStyling}>
+                    <View
+                        style={{
+                            maxWidth: '100%',
+                            flex: 1,
+                            backgroundColor: '#FFF',
+                            padding: 10,
+                            borderRadius: 10
+                        }}>
+                        <Barcode value="2620682025269" format="EAN13" flat />
+                        <Text style={text.monospace}>{this.state.cardNr}</Text>
+                    </View>
+                </View>
+                <Text style={[text.h1, text.textAlignCenter, account.extraMargin]}>
+                    Jouw Air Miles kaart
+                </Text>
+                <View style={account.inputPlacement}>
+                    <TextInput
+                        placeholder={'Vul Air Miles kaartnummer in...'}
+                        style={account.input}
+                        onChangeText={airMilesNr => this.setState({ airMilesNr })}
+                        onSubmitEditing={this.startSetAirMilesCardNumber()}
+                        placeholderTextColor="#838383"
+                        selectionColor="#ff7900"
+                        clearButtonMode="always"
+                        returnKeyType="go"
+                        keyboardType="decimal-pad"
+                    />
+                </View>
+                <TouchableOpacity
+                    style={buttons.button}
+                    onPress={this.startSetAirMilesCardNumber()}>
+                    <Text style={buttons.buttonText}>Aanpassen</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        );
+    }
 }
 
 export default SettingsScreen;
