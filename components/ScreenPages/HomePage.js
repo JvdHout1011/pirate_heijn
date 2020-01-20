82c1ef735d61495574e2289eef539d388a85bef4
JvdHout1011 <jjdkhout@avans.nl>
2020-01-20 15:21:52 +0100

    put async in query

--- components/ScreenPages/HomePage.js
+++ components/ScreenPages/HomePage.js
@@ -130,6 +130,8 @@ export default class HomeScreen extends React.Component {
         auth_cookie: this.newCookie
         })
           }
+
+        AsyncStorage.getItem('bonuskaart').then(async (bonus) => {
           console.log("cookie set")
         const cookieQuery = fs.collection('users').doc(this.state.discountCardNumber);
         const updateQuery = await cookieQuery.set({
@@ -139,6 +141,7 @@ export default class HomeScreen extends React.Component {
         AsyncStorage.setItem({auth_cookie: newCookie})
         this.setState({ auth_cookie: newCookie});
         })
+      })
       };
 
     _goToSettings = () => {
